02de04d86ec756bbaf06a3f642323df0adcd8561
Sargonnas <adelsignore@me.com>
2025-10-30 08:27:27 -0400

    Added exemption list by steamid and UI interface

--- SKO-GridPCULimitter/Patch/MergePatch.cs
+++ SKO-GridPCULimitter/Patch/MergePatch.cs
@@ -17,22 +17,26 @@ namespace SKO.GridPCULimiter.Patch
 
         private static bool MergeCheck(MyShipMergeBlock __instance)
         {
-            if (!SKOGridPCULimiterPlugin.Config.Enabled || SKOGridPCULimiterPlugin.Config.AllowMerge) return true;
+            if (!SKOGridPCULimiterPlugin.Instance.Config.Enabled || SKOGridPCULimiterPlugin.Instance.Config.AllowMerge) return true;
 
             if ((__instance != null ? __instance.Other : null) == null) return true;
 
             if (__instance.IsLocked || !__instance.IsFunctional || !__instance.Other.IsFunctional) return true;
 
-            if (SKOGridPCULimiterPlugin.Config.IgnoreNPCGrids)
+            if (SKOGridPCULimiterPlugin.Instance.Config.IgnoreNPCGrids)
             {
                 var ownerId = PlayerUtils.GetOwner(__instance.CubeGrid);
                 if (PlayerUtils.IsNpc(ownerId)) return true;
             }
 
-            if (GridUtils.GetPCU(__instance.CubeGrid, true, SKOGridPCULimiterPlugin.Config.IncludeConnectedGridsPCU) +
+            var gridOwnerId = PlayerUtils.GetOwner(__instance.CubeGrid);
+            var gridOwnerPlayer = PlayerUtils.GetPlayer(gridOwnerId);
+            if (gridOwnerPlayer != null && SKOGridPCULimiterPlugin.IsExemptFromPCULimit(gridOwnerPlayer.SteamUserId)) return true;
+
+            if (GridUtils.GetPCU(__instance.CubeGrid, true, SKOGridPCULimiterPlugin.Instance.Config.IncludeConnectedGridsPCU) +
                 GridUtils.GetPCU(__instance.Other.CubeGrid, true,
-                    SKOGridPCULimiterPlugin.Config.IncludeConnectedGridsPCU) >=
-                SKOGridPCULimiterPlugin.Config.MaxGridPCU)
+                    SKOGridPCULimiterPlugin.Instance.Config.IncludeConnectedGridsPCU) >=
+                SKOGridPCULimiterPlugin.Instance.Config.MaxGridPCU)
             {
                 // Disable it to avoid lagging.
                 __instance.Enabled = false;
